c066010d7e2ba1616a0cfea854feb11a3d7a5801
Trey Shugart <treshugart@gmail.com>
2015-09-01 12:54:50 +1000

    Update build and dist.

--- build/task/build.js
+++ build/task/build.js
@@ -5,13 +5,15 @@ var gulpConcat = require('gulp-concat');
 var gulpDebug = require('gulp-debug');
 var gulpFilter = require('gulp-filter');
 var gulpLess = require('gulp-less');
+var gulpMinifyCss = require('gulp-minify-css');
 var gulpRename = require('gulp-rename');
+var gulpUglify = require('gulp-uglify');
 
 module.exports = function () {
   var filterIcons = gulpFilter('*.?(eot|svg|ttf|woff|woff2)', { restore: true });
   var filterLess = gulpFilter('{**/*,*}.less', { restore: true });
   var filterJs = gulpFilter('{**/*,*}.js', { restore: true });
-  return gulp.src('src/lib/index.js')
+  return gulp.src('src/index.js')
     .pipe(gulpDebug({ title: 'trace' }))
 
     // All files are traced from the main js file and inserted into the stream.
@@ -22,6 +24,10 @@ module.exports = function () {
     .pipe(galv.cache('babel', gulpBabel()))
     .pipe(galv.cache('globalize', galv.globalize()))
     .pipe(gulpConcat('index.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(galv.cache('uglify', gulpUglify()))
+    .pipe(gulpConcat('index.min.js'))
+    .pipe(gulp.dest('dist'))
     .pipe(gulpDebug({ title: 'js' }))
     .pipe(filterJs.restore)
 
@@ -29,6 +35,10 @@ module.exports = function () {
     .pipe(filterLess)
     .pipe(galv.cache('less', gulpLess()))
     .pipe(gulpConcat('index.css'))
+    .pipe(gulp.dest('dist'))
+    .pipe(galv.cache('minify-css', gulpMinifyCss()))
+    .pipe(gulpConcat('index.min.css'))
+    .pipe(gulp.dest('dist'))
     .pipe(gulpDebug({ title: 'less' }))
     .pipe(filterLess.restore)
 
