05af3ae04c8bc127ae946eb47b84ab18850d2d86
Trey Shugart <treshugart@gmail.com>
2015-11-18 12:52:39 +1100

    Remove debounce and write some tests for the render() function.

--- src/render.js
+++ src/render.js
@@ -1,4 +1,3 @@
-import debounce from 'debounce';
 import createElement from './vdom/element';
 import merge from './merge';
 import mount from './vdom/mount';
@@ -13,20 +12,15 @@ export default function (render) {
       throw new Error('No node provided to diff renderer as either the first argument or the context.');
     }
 
-    if (!elem.__debouncedRender) {
-      elem.__debouncedRender = debounce(function (elem) {
-        const newTree = render(elem, { createElement });
-        if (elem.__oldTree) {
-          merge({
-            destination: newTree,
-            source: elem.__oldTree
-          });
-        } else {
-          mount(elem, newTree);
-        }
-        elem.__oldTree = newTree;
+    const newTree = render(elem, { createElement });
+    if (elem.__oldTree) {
+      merge({
+        destination: newTree,
+        source: elem.__oldTree
       });
+    } else {
+      mount(elem, newTree);
     }
-    elem.__debouncedRender(elem);
+    elem.__oldTree = newTree;
   };
 }
