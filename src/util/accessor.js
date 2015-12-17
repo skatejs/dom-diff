01dbe5dc586493248c875288afdaed0c8fa65db5
Trey Shugart <treshugart@gmail.com>
2015-12-17 16:46:59 +1100

    Update accessor code so that logic between the functions are consistent and fall back to using native DOM methods if available, else they fall further back to using the node like a virtual node.

--- src/util/accessor.js
+++ src/util/accessor.js
@@ -1,38 +1,23 @@
-function getAttribute (node, name) {
-  const attr = getAttributeObject(node, name);
-  return attr && attr.value;
-}
-
-function getAttributeObject (node, name) {
-  const attrs = node.attributes;
-  return attrs && attrs[name];
-}
-
-function removeAttribute (node, name) {
-  if (node.removeAttribute) {
-    node.removeAttribute(name);
-  } else {
-    const attrs = node.attributes;
-    if (attrs) {
-      delete attrs[name];
-    }
-  }
-}
-
-function setAttribute (node, name, value) {
-  const attr = getAttributeObject(node, name);
-  attr && (attr.value = value);
-}
-
 export function getAccessor (node, name) {
   if (name === 'class') {
     return node.className;
   } else if (name === 'style') {
     return node.style.cssText;
-  } else if (name !=='type' && name in node) {
+  } else if (name !== 'type' && name in node) {
     return node[name];
+  } else if (node.getAttribute) {
+    return node.getAttribute(name);
+  } else if (node.attributes && node.attributes[name]) {
+    return node.attributes[name].value;
   }
-  return getAttribute(node, name);
+}
+
+export function mapAccessor (node, name, value) {
+  if (name === 'class') {
+    node.className = value;
+  } else if (name === 'style') {
+    node.style = { cssText: value };
+  }
 }
 
 export function removeAccessor (node, name) {
@@ -42,8 +27,10 @@ export function removeAccessor (node, name) {
     node.style.cssText = '';
   } else if (name !== 'type' && name in node) {
     node[name] = '';
-  } else {
-    removeAttribute(node, name);
+  } else if (node.removeAttribute) {
+    node.removeAttribute(name);
+  } else if (node.attributes) {
+    delete node.attributes[name];
   }
 }
 
@@ -52,9 +39,11 @@ export function setAccessor (node, name, value) {
     node.className = value;
   } else if (name === 'style') {
     node.style.cssText = value;
-  } else if (name !=='type' && name in node) {
+  } else if (name !== 'type' && name in node) {
     node[name] = value;
-  } else {
-    setAttribute(node, name, value);
+  } else if (node.setAttribute) {
+    node.setAttribute(name, value);
+  } else if (node.attributes) {
+    node.attributes[node.attributes.length] = node.attributes[name] = { name, value };
   }
 }
